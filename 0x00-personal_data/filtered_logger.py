ff7cfb1506a2ef3fa7b14769d8e4b763fe39bb86
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2023-02-02 20:27:01 +0300

    task-0 done

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''Filtering logs's module
+'''
+from typing import List
+import re
+
+
+patterns = {
+    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
+    'replace': lambda x: r'\g<field>={}'.format(x),
+}
+
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """Filters a log line.
+    """
+    extract, replace = (patterns["extract"], patterns["replace"])
+    return re.sub(extract(fields, separator), replace(redaction), message)
