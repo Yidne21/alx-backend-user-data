fef90f5fe6cb8bed5fd339bd3fb432f800b5ac94
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2023-02-07 23:06:34 +0300

    add doc

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,10 +12,16 @@ class Auth:
     '''
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Checks if a path requires authentication.
+        """
         return False
 
     def authorization_header(self, request=None) -> str:
+        """Gets the authorization header field from the request.
+        """
         return request
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """Gets the current user from the request.
+        """
         return request
